e1ec1808b47cdbbc8f061fc9a3943ac62918b29d
root <root@docker-qual.dorz.fr>
2016-01-25 02:32:23 +0100

    initial commit

---
+++ data/srv/magento/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Set up</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<p>If you can read this, the web server is working.</p>
+		<?php
+			echo '<p>If you can read this, <a href="phpinfo.php">php</a> is working, too.</p>';
+			echo '<p>Now it&rsquo;s time to install Magento. Run the <code>tools</code> container.</p>'; ?>
+	</body>
+</html>
